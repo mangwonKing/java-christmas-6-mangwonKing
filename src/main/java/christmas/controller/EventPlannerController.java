edb3be3350833df4be939d47bc24b5f80a187de4
mangwonKing <aud1035@naver.com>
2023-11-12 22:06:29 +0900

    feat(cgristmas) 메뉴를 입력받아 order 에 정상적으로 저장하는 기능 구현

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -1,6 +1,7 @@
 package christmas.controller;
 
 import christmas.model.DateInfomation;
+import christmas.model.OrderInfomation;
 import christmas.view.InputView;
 
 public class EventPlannerController {
@@ -10,7 +11,8 @@ public class EventPlannerController {
         int date = inputView.readDate();
         DateInfomation dateInfomation = new DateInfomation(date);
         dateInfomation.checkDay();
-
+        String order = InputView.readMenu();
+        OrderInfomation orderInfomation = new OrderInfomation(order);
 
     }
     public void run(){
--- src/main/java/christmas/model/OrderInfomation.java
+++ src/main/java/christmas/model/OrderInfomation.java
@@ -1,12 +1,27 @@
 package christmas.model;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class OrderInfomation {
 
-    private Map<String,Integer> order;
+    private Map<String,String> order = new HashMap<>();
     private int totalPrice;
 
+
+    public OrderInfomation(String menu){
+        List<String> menus = Arrays.asList(menu.split(","));
+        System.out.println(menus);
+
+        for(String getMenu : menus){
+            String[] menuInfo =  getMenu.split("-");
+            //예외처리
+            order.put(menuInfo[0],menuInfo[1]);
+        }
+        System.out.println(order.keySet());
+    }
     public void checkCategory(){
 
     }
