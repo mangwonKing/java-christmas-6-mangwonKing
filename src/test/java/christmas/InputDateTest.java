651fa9bf84476df2b4aff6694a921bae9b672a9e
mangwonKing <aud1035@naver.com>
2023-11-15 20:31:20 +0900

    test*christmas) 입력받은 날이 주말인지 확인하는 주말_확인 테스트코드 추가

---
+++ src/test/java/christmas/InputDateTest.java
@@ -0,0 +1,22 @@
+package christmas;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import christmas.model.DateInfomation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class InputDateTest {
+    private DateInfomation dateInfomation;
+    private int weekendTestDay = 2;
+    private int normalTestDay = 4;
+    private int specialTestDay = 3;
+
+    @Test
+    void 주말_확인(){
+        dateInfomation = new DateInfomation(weekendTestDay);
+        boolean result = dateInfomation.getIsWeekend();
+        assertThat(result).isEqualTo(true);
+    }
+}
