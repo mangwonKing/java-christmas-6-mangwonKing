f0f7cd1f29232e25900b8aa4c53d7f5ffee74ee5
mangwonKing <aud1035@naver.com>
2023-11-15 20:33:49 +0900

    test*christmas) 입력받은 날이 평일인지 확인하는 평일_확인 테스트코드 추가

--- src/test/java/christmas/InputDateTest.java
+++ src/test/java/christmas/InputDateTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import christmas.model.DateInfomation;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 public class InputDateTest {
     private DateInfomation dateInfomation;
@@ -18,5 +19,11 @@ public class InputDateTest {
         dateInfomation = new DateInfomation(weekendTestDay);
         boolean result = dateInfomation.getIsWeekend();
         assertThat(result).isEqualTo(true);
+    }
+    @Test
+    void 평일_확인(){
+        dateInfomation = new DateInfomation(normalTestDay);
+        boolean result = dateInfomation.getIsWeekend();
+        assertThat(result).isEqualTo(false);
     }
 }
