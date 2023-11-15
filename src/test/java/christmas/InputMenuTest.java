048daa4c0fc5b4c87b3ef7aab8ff550fc6c59e6f
mangwonKing <aud1035@naver.com>
2023-11-15 20:56:30 +0900

    test(christmas) 카테고리를 판별하는 test 코드 구현 및 InputMenuTest 에서 필요한 인스턴스 초기화를 setUp 메서드로 분리

--- src/test/java/christmas/InputMenuTest.java
+++ src/test/java/christmas/InputMenuTest.java
@@ -2,6 +2,7 @@ package christmas;
 
 import christmas.model.OrderInfomation;
 import christmas.view.InputView;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -14,15 +15,24 @@ public class InputMenuTest {
 
     private OrderInfomation orderInfomation;
     private Map<String,Integer> order = new HashMap<>();
-    @Test
-    void 총주문금액계산(){
+    @BeforeEach
+    void setUp(){
         order.put("티본스테이크",1);
         order.put("바비큐립",1);
         order.put("초코케이크",2);
         order.put("제로콜라",1);
 
         orderInfomation = new OrderInfomation(order);
+    }
+    @Test
+    void 총주문금액계산(){
+
         int result = orderInfomation.calculateTotal();
         assertThat(result).isEqualTo(142000);
+    }
+    @Test
+    void 카테고리판별(){
+        String result = orderInfomation.getCategory("티본스테이크");
+        assertThat(result).isEqualTo("메인");
     }
 }
