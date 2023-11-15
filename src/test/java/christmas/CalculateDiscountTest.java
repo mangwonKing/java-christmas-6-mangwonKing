625f3ac342fc09e41b8d71d39dec825a6d5ff945
mangwonKing <aud1035@naver.com>
2023-11-15 21:11:35 +0900

    test(christmas) 디데이 할인 계산을 테스트 하는 코드 추가

---
+++ src/test/java/christmas/CalculateDiscountTest.java
@@ -0,0 +1,28 @@
+package christmas;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import christmas.model.DateInfomation;
+import christmas.model.Discount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class CalculateDiscountTest {
+
+    private Discount discount;
+    private DateInfomation dateInfomation;
+    private final int testDate = 10;
+    @BeforeEach
+    void setUp(){
+        discount = new Discount();
+        dateInfomation = new DateInfomation(testDate);
+    }
+
+    @Test
+    void 디데이_할인_계산(){
+        int result = discount.calculateDayDiscount(dateInfomation);
+        assertThat(result).isEqualTo(1900);
+    }
+
+}
