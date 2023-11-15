3205b515d97b05f0756ae33aca0c2c5d20de2bf9
mangwonKing <aud1035@naver.com>
2023-11-15 22:01:06 +0900

    test(christmas) 메뉴주문 양식이 잘못된 경우에  대한 예외처리 테스트코드 추가

--- src/test/java/christmas/ExceptionTest.java
+++ src/test/java/christmas/ExceptionTest.java
@@ -24,8 +24,14 @@ public class ExceptionTest extends NsTest {
             assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         });
     }
-    // 메뉴를 1개이상 시켰는지
-
+    // 메뉴주문 양식이 올바르지 않은 경우
+    @Test
+    void 메뉴_잘못된_양식_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("4","해산물파스타+1,제로콜라+2");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
     //총 주문 개수가 20개를 넘기지 않았는지
 
     //음료만 주문하지는 않았는지
