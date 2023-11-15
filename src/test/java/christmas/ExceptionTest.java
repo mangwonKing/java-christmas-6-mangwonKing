4938d4349ed8418d6929f4e4a4cb03f58b859b96
mangwonKing <aud1035@naver.com>
2023-11-15 21:58:10 +0900

    test(christmas) 존재하지 않는 메뉴에 대한 예외처리 테스트코드 추가

--- src/test/java/christmas/ExceptionTest.java
+++ src/test/java/christmas/ExceptionTest.java
@@ -9,7 +9,13 @@ import org.junit.jupiter.api.Test;
 public class ExceptionTest extends NsTest {
 
     // 메뉴가 존재 하는지
-
+    @Test
+    void 메뉴_존재_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("3","해순물파스타-1");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
     // 메뉴가 중복은 아닌지
 
     // 메뉴를 1개이상 시켰는지
@@ -17,6 +23,8 @@ public class ExceptionTest extends NsTest {
     //총 주문 개수가 20개를 넘기지 않았는지
 
     //음료만 주문하지는 않았는지
+
+    //1 이상 31 이하의 숫자가 아닌 다른 수가 입력되는 경우
     @Override
     protected void runMain() {
         Application.main(new String[]{});
